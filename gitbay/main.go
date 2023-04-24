3da51f8b7e9acddd4aad16effbf32d604085eed6
Axel Gard <axel.gard@protonmail.com>
2023-04-24 10:33:38 +0200

    libp2p hello world

--- gitbay/main.go
+++ gitbay/main.go
@@ -1,7 +1,22 @@
 package main
-import "fmt"
 
+import (
+    "fmt"
+    "github.com/libp2p/go-libp2p"
+)
 
 func main() {
-   fmt.Println("Hello gitbay") 
+    // start a libp2p node with default settings
+    node, err := libp2p.New()
+    if err != nil {
+        panic(err)
+    }
+
+    // print the node's listening addresses
+    fmt.Println("Listen addresses:", node.Addrs())
+
+    // shut the node down
+    if err := node.Close(); err != nil {
+        panic(err)
+    }
 }
