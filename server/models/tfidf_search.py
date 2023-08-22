b4184394c03a42399259aabab48edf39488f1387
Axel Gard <axel.gard@protonmail.com>
2023-08-22 10:37:19 +0200

    fixed tf-idf to use DF for faster search

--- server/models/tfidf_search.py
+++ server/models/tfidf_search.py
@@ -8,13 +8,13 @@ from .metadata import apply_metadata
 
 
 class TfIdf(SearchModel):
-    def __init__(self, prob_cut_of: float = 0.1, clean: bool = True) -> None:
+    def __init__(self, cut_of: float = 0.1, clean: bool = True) -> None:
         super().__init__()
         self.vectorizer = TfidfVectorizer()
         self.features = None
         self.files, self.contents = util.load_data()
         self.tf_idf = None
-        self.prob_cut_of = prob_cut_of
+        self.cut_of = cut_of
         self.clean = clean
 
     def train(self) -> None:
@@ -23,28 +23,21 @@ class TfIdf(SearchModel):
         self.feature_names = self.vectorizer.get_feature_names_out()
         dense = self.features.todense()
         self.model = dense.tolist()
-        self.tf_idf = pd.DataFrame(self.model, columns=self.feature_names).to_dict()
+        self.tf_idf = pd.DataFrame(self.model, columns=self.feature_names)
 
-    def query_proba(self, q: str) -> list:
-        assert (
-            self.tf_idf != None
-        ), "the model needs to be trained before running queries "
+    def query_proba(self, q: str) -> dict:
         if self.clean:
             q = util.clean([q])[0]
-        words_tf_idf = {}
-        query_words = q.split(" ")
-        for word in query_words:
-            if word in self.tf_idf.keys():
-                words_tf_idf[word] = self.tf_idf[word]
-        result = [0] * len(self.files)
-        _sum = 0.0
-        for word, tf_idf in words_tf_idf.items():
-            for idx, val in tf_idf.items():
-                result[idx] += val
-                _sum += val
+        result = []
+        tf_idf = self.tf_idf.copy()
+        q = q.split(" ")
+        q = tf_idf.columns[tf_idf.columns.isin(q)]
+        tf_idf = tf_idf[q].T
+        document_wights = tf_idf.sum(axis=0)
+        _sum = document_wights.sum()
         if _sum == 0.0:
             _sum = 1
-        result = [v / _sum for v in result]
+        result = [v / _sum for v in document_wights.values]
         return result
 
     def query(self, q: str) -> list:
@@ -53,7 +46,7 @@ class TfIdf(SearchModel):
         intermediate_results = []
         idxs = np.array(p_result).argsort()
         for i in idxs:
-            if p_result[i] > self.prob_cut_of:
+            if p_result[i] > self.cut_of:
                 intermediate_results.append([p_result[i],self.files[i]])
         intermediate_results.sort()
         intermediate_results.reverse()
